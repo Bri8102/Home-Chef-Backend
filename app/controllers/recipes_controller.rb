44f0960b3bbd832dce2deba496055ab939757c1e
Bri8102 <damioniru16@gmail.com>
2021-01-25 17:50:35 +0100

    updated routes for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,24 @@ class RecipesController < ApplicationController
         recipes = Recipe.all
         render json: RecipeSerializer.new(recipes)
     end
+
+    def show
+        recipes = Ingredient.find_by(name: params[:ingredient_name]).recipes
+    
+        render json: RecipeSerializer.new(recipes.sample)
+      end
+    
+      def create
+        recipe = Recipe.new(recipe_params)
+        ingredients = params[:ingredients].map { |ingredient| Ingredient.find_or_create_by(name: ingredient) }
+        recipe.ingredients << ingredients
+        recipe.save
+        render json: RecipeSerializer.new(recipe)
+      end
+    
+      private
+    
+        def recipe_params
+          params.require(:recipe).permit(:title, :recipe_link, :image_link)
+        end
 end
