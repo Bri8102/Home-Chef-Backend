30ff21b74d4584bfdbe51908053d4cad07e86207
Bri8102 <damioniru16@gmail.com>
2021-01-26 21:42:45 +0100

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :recipes, only: [:index, :show, :create]
-  resources :ingredients
+  resources :recipes, only: [:index, :create]
+  # get '/recipes/:ingredient_name' => 'recipes#show'
+  # resources :ingredients, only: [:index]
+
+  get '/recipes' => 'recipes#index'
+  get '/recipes/:ingredient_name' => 'recipes#show'
+  post '/recipes' => 'recipes#create'
+
+  get '/ingredients' => 'ingredients#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
